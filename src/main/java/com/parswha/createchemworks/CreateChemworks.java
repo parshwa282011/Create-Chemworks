fc745d0f1f3548615c69b6b31f27fabc54e8ed87
parshwa282011 <parshwashah282011@gmail.com>
2026-07-18 12:00:45 +0530

    base code

---
+++ src/main/java/com/parswha/createchemworks/CreateChemworks.java
@@ -0,0 +1,12 @@
+package com.parswha.createchemworks;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(CreateChemworks.MOD_ID)
+public final class CreateChemworks {
+    public static final String MOD_ID = "create_chemworks";
+
+    public CreateChemworks() {
+        // Register content and event listeners here.
+    }
+}
